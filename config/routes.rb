a36acdb6381a863ab95484cfa34c62b38315e6d7
kreopelle <kreopelle@gmail.com>
2018-11-01 17:40:47 -0400

    Create sound model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resource :sound, except: [:new, :edit]
+  end
 end
